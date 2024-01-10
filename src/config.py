50c110f4d2b2311d2f872487d92e6d5c2f6b6069
gamingflexer <osurve20ece@student.mes.ac.in>
2024-01-10 05:12:21 +0000

    Add config file with dense models

---
+++ src/config.py
@@ -0,0 +1,19 @@
+from decouple import config
+import os
+
+OPENAI_API_KEY = config('OPENAI_API_KEY', default="")
+
+dense_models = {
+    'text-embedding-ada-002': {
+        'source': 'openai',
+        'dimension': 1536,
+        'api_key': True,
+        'metric': 'dotproduct'
+    },
+    'multilingual-22-12': {
+        'source': 'cohere',
+        'dimension': 768,
+        'api_key': True,
+        'metric': 'dotproduct'
+    }
+}

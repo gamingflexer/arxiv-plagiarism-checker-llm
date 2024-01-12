9169c2aadee7e6fc44420b788625d60ea54e50b0
gamingflexer <osurve20ece@student.mes.ac.in>
2024-01-12 05:33:04 +0000

    Add SERP API key to config.py

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ from decouple import config
 import os
 
 OPENAI_API_KEY = config('OPENAI_API_KEY', default="")
+SERP_API_KEY = config('SERP_API_KEY', default="")
 
 dense_models = {
     'text-embedding-ada-002': {

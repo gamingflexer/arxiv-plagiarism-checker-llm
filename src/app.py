5718d42b5e034485937448121fdaa30c7d5eb967
gamingflexer <osurve20ece@student.mes.ac.in>
2024-01-17 19:56:47 +0000

    Add unique authors dataframe to the plagiarism checker

--- src/app.py
+++ src/app.py
@@ -3,15 +3,10 @@ import pandas as pd
 import logging
 from scrapper.main import ArxivPaper
 from config import *
-from db.db_functions import get_correct_author_name, insert_papers_data, fetch_papers_data
+from db.db_functions import get_correct_author_name, insert_papers_data, fetch_papers_data, get_unquine_authors
 from utils import compare_paper_ids
 
-"""
-author_obj = ArxivPaper("Andrew Ng")
-paper_links = author_obj.get_results_google(number_of_results=25)
-paper_ids = author_obj.get_paper_id(paper_links)
-author_obj.get_paper_details_batch(paper_ids=paper_ids, path="./data/papers")
-"""
+unique_authors_df = get_unquine_authors()
 
 def plagiarism_checker(authors_name_fetch,number_of_results_fetch, progress=gr.Progress()):
     number_of_results_fetch = int(number_of_results_fetch)
@@ -21,26 +16,29 @@ def plagiarism_checker(authors_name_fetch,number_of_results_fetch, progress=gr.P
     db_author_name = get_correct_author_name(authors_name_fetch)
     paper_links = author_obj.get_results_google(number_of_results=number_of_results_fetch)
     paper_ids = author_obj.get_paper_id(paper_links)
-    progress(0.4, desc="Collecting Papers")
+    progress(0.4, desc=f"Collecting Papers for {len(paper_ids)}")
     if db_author_name is None:
+        data = fetch_papers_data(db_author_name)
+        remaining_paper_ids = compare_paper_ids(data,paper_ids)
         print("No similar author found in the database")
-        author_obj.get_paper_details_batch(paper_ids=paper_ids, path="./data/papers")
         local_saved_papers = os.path.join(os.getcwd(), "data", "papers", authors_name_fetch.replace(" ", "_"))
         progress(0.6, desc="Making summary")
         data_to_save = []
-        for paper in os.listdir(local_saved_papers):
-            paper_path = os.path.join(local_saved_papers, paper)
-            with open(paper_path, "r") as f:
-                data_to_save.append(f.read())
+        if remaining_paper_ids != []:
+            author_obj.get_paper_details_batch(paper_ids=paper_ids, path="./data/papers")
+            for paper in os.listdir(local_saved_papers):
+                paper_path = os.path.join(local_saved_papers, paper)
+                with open(paper_path, "r") as f:
+                    data_to_save.append(f.read())
     else:
         print(f"Found similar author in the database: {db_author_name}")
         data = fetch_papers_data(db_author_name)
         remaining_paper_ids = compare_paper_ids(data,paper_ids)
+        local_saved_papers = os.path.join(os.getcwd(), "data", "papers", authors_name_fetch.replace(" ", "_"))
         progress(0.6, desc="Making summary")
         data_to_save = []
         if remaining_paper_ids != []:
             author_obj.get_paper_details_batch(paper_ids=remaining_paper_ids, path="./data/papers")
-            local_saved_papers = os.path.join(os.getcwd(), "data", "papers", authors_name_fetch.replace(" ", "_"))
             for paper in os.listdir(local_saved_papers):
                 paper_path = os.path.join(local_saved_papers, paper)
                 with open(paper_path, "r") as f:
@@ -50,7 +48,7 @@ def plagiarism_checker(authors_name_fetch,number_of_results_fetch, progress=gr.P
         
     progress(0.8, desc="Saving to Database")
     insert_papers_data(data_to_save, authors_name_fetch)
-    return "Fetched Latest Papers"
+    return f"Fetched Latest Papers for {len(remaining_paper_ids)}"
 
 def fetch_papers_data_df(authors_name: str, progress=gr.Progress()):
     progress(0.2, desc="Fetching Papers")
@@ -70,6 +68,9 @@ with gr.Blocks() as demo:
             dataframe_output = gr.Dataframe(headers=['doi_no', 'author_name', 'title', 'authors', 'year', 'pdf_link',
     'references', 'categories', 'comment', 'journal_ref', 'source',
     'summary', 'published'])
+        with gr.Row():
+            unquine_authors_output = gr.Dataframe(headers=["author_name"],value=unique_authors_df, label=" Authors Currently in our DB")
+        
         
     with gr.Tab("Arxiv Plagiarism Fetcher & Save to DB"):
         with gr.Row():

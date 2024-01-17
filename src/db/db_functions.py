ce5b6dd6bef31f49ecea728e8c608ebd04c90c15
gamingflexer <osurve20ece@student.mes.ac.in>
2024-01-17 19:56:55 +0000

    Add function to get unique authors

--- src/db/db_functions.py
+++ src/db/db_functions.py
@@ -6,6 +6,7 @@ from fuzzywuzzy import fuzz
 from config import SUPABASE_URL, SUPABASE_KEY
 import json
 import logging
+import pandas as pd 
 
 url: str = SUPABASE_URL
 key: str = SUPABASE_KEY
@@ -53,7 +54,13 @@ def get_correct_author_name(user_input_author):
 def fetch_papers_data(author_name, fields_to_query = ["doi_no"],table_name: str = 'papers', all=False):
     author_name = get_correct_author_name(author_name)
     if all:
-        data, count = supabase.table(table_name).select("*").eq('author_name', author_name).execute()
+        data, count = supabase.table(table_name).select("*").execute()
         return data
     data, count = supabase.table(table_name).select(",".join(fields_to_query)).eq('author_name', author_name).execute()
     return data[1]
+
+def get_unquine_authors():
+    authors_name_data = supabase.table('papers').select('author_name').execute()
+    unique_authors = set(author_dict['author_name'] for author_dict in authors_name_data.data)
+    unique_authors_df = pd.DataFrame(unique_authors, columns=['author_name'])
+    return unique_authors_df
